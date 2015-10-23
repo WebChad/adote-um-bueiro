aa68c7c39dcc30ddb4ccb0d0cbbdaea07b005713
Neri Junior <neri@nerijunior.com>
2015-10-23 20:21:26 -0200

    traducao

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,6 @@ protected
     available_languages = Dir.glob(Rails.root + 'config/locales/??.yml').collect do |file|
       File.basename(file, '.yml')
     end
-    I18n.locale = http_accept_language.compatible_language_from(available_languages) || I18n.default_locale
+    # I18n.locale = http_accept_language.compatible_language_from(available_languages) || I18n.default_locale
   end
 end
